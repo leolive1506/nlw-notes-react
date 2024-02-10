3e9f90127ad9a397dc4318343f502f89fb285db3
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-02-10 12:52:11 -0300

    feat: add dynamic notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note = {
-  date: new Date(),
-  content: 'Hello world'
+export interface Note {
+  id: string
+  date: Date,
+  content: string
 }
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+    setNotes(prev => [newNote, ...prev])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Experts"/>
@@ -21,10 +34,10 @@ export function App() {
       <div className="h-px bg-slate-700"></div>
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <NewNoteCard />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {notes.map(note => (
+          <NoteCard note={note} key={note.id} />
+        ))}
       </div>
     </div>
   )
