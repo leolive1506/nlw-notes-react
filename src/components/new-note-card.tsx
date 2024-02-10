d18cbcf1bd763dd5c3ea2816ee30a29e8df8933a
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-02-10 12:21:17 -0300

    feat: handle user typing note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,20 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
+import { ChangeEvent, useState } from 'react'
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === '') {
+      setShouldShowOnboarding(true)
+    }
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md bg-slate-700 p-5 flex flex-col gap-3 text-left overflow-hidden hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
@@ -21,9 +34,17 @@ export function NewNoteCard() {
               Adicionar nota    
             </span>
 
-            <p className="text-sm leading-6 text-slate-400">
-              Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>
-            </p>  
+            {shouldShowOnboarding ? (
+              <p className="text-sm leading-6 text-slate-400">
+                Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button className="font-medium text-lime-400 hover:underline" onClick={handleStartEditor}>utilize apenas texto</button>
+              </p>  
+            ): (
+              <textarea
+                autoFocus
+                className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChanged}
+              />
+            )}
           </div>
 
           <button
